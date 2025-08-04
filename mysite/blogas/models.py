cabdb8a0b67169f2f1e90aa19fcfb0c389906c06
DonatasNoreika <donoras@gmail.com>
2025-08-04 19:28:18 +0300

    Perjungta į LT kalbą, nuskatytas Post modelio pavadinimas lietuvių kalba

--- mysite/blogas/models.py
+++ mysite/blogas/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    body = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(verbose_name="Pavadinimas", max_length=200)
+    body = models.TextField(verbose_name="Turinys")
+    created_at = models.DateTimeField(verbose_name="Sukurta", auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Įrašas"
+        verbose_name_plural = "Įrašai"
 
     def __str__(self):
         return self.title
