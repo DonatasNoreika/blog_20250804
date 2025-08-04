ab6ee480988cff0c2255c605ee08e23fa0a031a7
DonatasNoreika <donoras@gmail.com>
2025-08-04 19:41:40 +0300

    Pridėtas naujas boolean laukas, admin.py nustatyti list_editable

--- mysite/blogas/admin.py
+++ mysite/blogas/admin.py
@@ -4,15 +4,16 @@ from .models import Post
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'created_at']
+    list_display = ['id', 'title', 'published', 'created_at']
     ordering = ['-created_at']
     search_fields = ['title', 'body']
     list_filter = ['created_at']
     readonly_fields = ['id', 'created_at']
+    list_editable = ['title', 'published']
 
     fieldsets = [
         ("General", {"fields": ("title", "body")}),
-        ("Technical", {"fields": ("id", "created_at")}),
+        ("Technical", {"fields": ("id", "created_at", 'published')}),
     ]
 
 admin.site.register(Post, PostAdmin)
--- mysite/blogas/models.py
+++ mysite/blogas/models.py
@@ -5,6 +5,7 @@ class Post(models.Model):
     title = models.CharField(verbose_name="Pavadinimas", max_length=200)
     body = models.TextField(verbose_name="Turinys")
     created_at = models.DateTimeField(verbose_name="Sukurta", auto_now_add=True)
+    published = models.BooleanField(verbose_name="Paskelbta", default=True)
 
     class Meta:
         verbose_name = "Įrašas"
