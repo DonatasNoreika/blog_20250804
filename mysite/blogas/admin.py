dda8ddb2a0a8db5bb6e1ccac55fba7c7d2b72652
DonatasNoreika <donoras@gmail.com>
2025-08-04 18:51:07 +0300

    Į PostAdmin įdėti search_fields, list_filter

--- mysite/blogas/admin.py
+++ mysite/blogas/admin.py
@@ -6,5 +6,7 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'created_at']
     ordering = ['-created_at']
+    search_fields = ['title', 'body']
+    list_filter = ['created_at']
 
 admin.site.register(Post, PostAdmin)
